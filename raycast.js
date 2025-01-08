00c5b9f75382c6684bb9c85268debaf99f1b2712
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-07 21:49:10 -0300

    Add player movement and rendering functionality
    Introduced a `Player` class with movement and rendering capabilities, including updating position and rotation based on user input. Added event handling for arrow key presses to control player turning and walking. Updated the `draw` function to integrate the player's state updates and rendering.

--- raycast.js
+++ raycast.js
@@ -22,6 +22,7 @@ class Map {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
   }
+
   render() {
     for (let i = 0; i < MAP_NUM_ROWS; i++) {
       for (let j = 0; j < MAP_NUM_COLS; j++) {
@@ -36,21 +37,73 @@ class Map {
   }
 }
 
-let grid = new Map();
+class Player {
+  constructor() {
+    this.x = WINDOW_WIDTH / 2;
+    this.y = WINDOW_HEIGHT / 2;
+    this.radius = 10;
+    this.turnDirection = 0; // -1 left or +1 right
+    this.walkDirection = 0; // -1 back or +1 front
+    this.rotationAngle = Math.PI / 2;
+    this.moveSpeed = 2.0;
+    this.rotationSpeed = 2 * (Math.PI / 180);
+  }
 
-// Initialize all objects
+  render() {
+    noStroke();
+    fill("red");
+    circle(this.x, this.y, this.radius);
+    stroke("red");
+    line(this.x, this.y, this.x + (30 * Math.cos(this.rotationAngle)), this.y + 30 * Math.sin(this.rotationAngle));
+  }
+
+  update() {
+    this.rotationAngle += this.turnDirection * this.rotationSpeed;
+
+    let moveStep = this.walkDirection * this.moveSpeed;
+    this.x += moveStep * Math.cos(this.rotationAngle);
+    this.y += moveStep * Math.sin(this.rotationAngle);
+  }
+}
+
+let grid = new Map();
+let player = new Player();
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    player.turnDirection = -1;
+  } else if (keyCode === RIGHT_ARROW) {
+    player.turnDirection = +1;
+  } else if (keyCode === UP_ARROW) {
+    player.walkDirection = +1;
+  } else if (keyCode === DOWN_ARROW) {
+    player.walkDirection = -1;
+  } else {
+    console.log("Unknown key pressed");
+  }
+}
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
+    player.turnDirection = 0;
+  } else if (keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+    player.walkDirection = 0;
+  }
+}
+
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 }
 
 function update() {
-
+  player.update()
 }
 
 function draw() {
   update();
   grid.render();
+  player.render();
 }
 
 
